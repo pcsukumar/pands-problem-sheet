704632bf194e9f3351ec6ceda788f509bd1a90fe
Prasanth Sukumar <sukumar@outlook.ie>
2022-01-30 11:02:16 +0000

    Comment updated

--- bmi.py
+++ bmi.py
@@ -1,6 +1,9 @@
 #This program is to calculate the BMI when weight (Kg) and height (cm) given.
 #Author : Prasanth Sukumar
-weight = float(input('Weight (kg): ')) #Asking user to input weight
-height = float(input('Height (cm): ')) #Asking user to input height
+
+weight = float(input('Weight (kg): ')) #Asking user to input weight and convert the value into float
+height = float(input('Height (cm): ')) #Asking user to input height and convert the value into float
+
 bmi = round(weight/(height/100)**2, 2)  # Found how to round to 2 decimal places form this page https://stackoverflow.com/questions/20457038/how-to-round-to-2-decimals-with-python
+
 print("Your BMI is:{}" .format(bmi))
