003008c1d93484abde24b3e60d59af634ff4f24e
Prasanth Sukumar <sukumar@outlook.ie>
2022-01-30 11:15:00 +0000

    Formating chaged

--- bmi.py
+++ bmi.py
@@ -4,6 +4,6 @@
 weight = float(input('Weight (kg): ')) #Asking user to input weight and convert the value into float
 height = float(input('Height (cm): ')) #Asking user to input height and convert the value into float
 
-bmi = round(weight/(height/100)**2, 2)  # Found how to round to 2 decimal places form this page https://stackoverflow.com/questions/20457038/how-to-round-to-2-decimals-with-python
+bmi = weight/(height/100)**2  # Calculate BMI
 
-print("Your BMI is (kg/m2):{}" .format(bmi))
+print("Your BMI is (kg/m2): {:.2f}" .format(bmi)) # ':.2f' used to round BMI value to two decimels
