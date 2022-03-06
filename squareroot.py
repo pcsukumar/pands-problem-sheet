140dc12141fbd79e78e5c264f66afbc1fc7d5dcc
Prasanth Sukumar <sukumar@outlook.ie>
2022-03-06 10:42:47 +0000

    Find approximate squareroot

--- squareroot.py
+++ squareroot.py
@@ -3,7 +3,7 @@
 
 #Source: slightly updated the code found in this video https://www.youtube.com/watch?v=tUFzOLDuvaE
 
-def approxSqrt(num):
+def sqrt(num):
 	guess = num					# First guess is just num itself
 	diff = 999999999			# diff is used to check terminating condition
 	while diff > 0.0001:        # Loop until difference is too small
@@ -21,5 +21,5 @@ def approxSqrt(num):
 	return guess
 	
 num = float(input("Please enter a positive number:"))
-sqrt = approxSqrt(num)
-print("The square root of {} is approx. {:.1f}".format(num, sqrt))
+sqroot = sqrt(num)
+print("The square root of {} is approx. {:.1f}".format(num, sqroot))
