e9dd50a45696e62d33ea1e581e47b34c3e13d3ac
Prasanth Sukumar <sukumar@outlook.ie>
2022-04-03 10:42:57 +0100

    Code updated to use precision

--- squareroot.py
+++ squareroot.py
@@ -9,7 +9,6 @@ def sqrt(number):
     while diff > 0.05: # iterate until difference between original number and squrare of guessed number is less than 0.05
         number = 0.5 * (number + a / number) # update using newtowns method x_(n+1) = 0.5 * (x_n +a / x_n)
         diff = abs(number**2-a)
-        print (diff)
     return number
 
 num = float(input("Please enter a positive number:"))
