b3624bf2e1658923d431e7b44f709afa015ea6e5
Prasanth Sukumar <sukumar@outlook.ie>
2022-04-04 07:46:38 +0100

    Update

---
+++ plottask.py
@@ -0,0 +1,5 @@
+#This program displays a plot of the functions f(x)=x, g(x)=x2 and h(x)=x3 in the range [0, 4] on the one set of axes.
+#Author : Prasanth Sukumar
+
+import numpy as np
+import matplotlib.pyplot as plt
