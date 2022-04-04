b7e642d14f90f5685fe834e28a38b7b030897bff
Prasanth Sukumar <sukumar@outlook.ie>
2022-04-04 18:24:06 +0100

    Code updated to add legend

--- plottask.py
+++ plottask.py
@@ -6,16 +6,19 @@ import matplotlib.pyplot as plt
 
 xlist = np.linspace(0,4,100)
 ylist = xlist
-plt.plot(xlist, ylist)
+plt.plot(xlist, ylist, label="f(x)=x")
 
 ylist = xlist**2
-plt.plot(xlist, ylist)
+plt.plot(xlist, ylist, label="g(x)=X2")
 
 ylist = xlist**3
-plt.plot(xlist, ylist)
+plt.plot(xlist, ylist, label="g(x)=X3")
 
 plt.title('Plot of the functions f(x)=x, g(x)=x2 and h(x)=x3 in the range [0, 4]')
 plt.xlabel('x - axis')
 plt.ylabel('y - axis')
 plt.legend() # show a legend on the plot
 plt.show()
+
+#Reference
+# 1. Adding a legend https://stackoverflow.com/questions/19125722/adding-a-legend-to-pyplot-in-matplotlib-in-the-simplest-manner-possible
