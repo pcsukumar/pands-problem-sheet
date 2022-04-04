c11821a5dc70b81b4342b1da59f628420ba58426
Prasanth Sukumar <sukumar@outlook.ie>
2022-04-04 08:08:39 +0100

    Code update

--- plottask.py
+++ plottask.py
@@ -3,3 +3,19 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+xlist = np.linspace(0,4,100)
+ylist = xlist
+plt.plot(xlist, ylist)
+
+ylist = xlist**2
+plt.plot(xlist, ylist)
+
+ylist = xlist**3
+plt.plot(xlist, ylist)
+
+plt.title('Plot of the functions f(x)=x, g(x)=x2 and h(x)=x3 in the range [0, 4]')
+plt.xlabel('x - axis')
+plt.ylabel('y - axis')
+plt.legend() # show a legend on the plot
+plt.show()
